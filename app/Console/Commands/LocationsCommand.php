2e57650cd3bc44a58b4a3149994caba16aacc5fc
andy-mac <requinsoft@gmail.com>
2024-12-04 17:05:35 -0600

    Validacion de grupos en FS

--- app/Console/Commands/LocationsCommand.php
+++ app/Console/Commands/LocationsCommand.php
@@ -33,18 +33,46 @@ class LocationsCommand extends Command
         $user = Auth::user();
         $fecha = Carbon::now();
         $hora = $fecha->format('H:i:s');
-        $url = 'https://app.fatiguescience.com/readi_api/v1/';
+        $url = 'https://app.fatiguescience.com/readi_api/v1/locations';
         $token = Fsdata::find(1);
+        $locationsOnFS = [];
 
-        $queryLocations = Http::withToken($token->access_token)->get($url.'locations');
+        $queryLocations = Http::withToken($token->access_token)->get($url);
         $responseLocations = $queryLocations['data'];
         
         foreach($responseLocations as $locations){
-            //print($locations['id']);
-            $locations = Locations::updateOrCreate([
-                'id_fs' => $locations['id'],
-                'name' => $locations['attributes']['name']
-            ]);
+            //print($locations['attributes']['name']);
+
+            //Guardamos los ids de los Locations en FS en el array LocationsOnFS
+            $locationsOnFS[] = $locations['id'];
+            $validarExistencia = Locations::query()->where('id_fs', $locations['id'])->count();
+
+            if($validarExistencia == 0){
+                $locations = Locations::create([
+                    'id_fs' => $locations['id'],
+                    'name' => $locations['attributes']['name']
+                ]);
+            }else{
+                $locationOnDB = Locations::query()->where('id_fs', $locations['id'])->get();
+
+                if($locations['attributes']['name'] != $locationOnDB[0]->name){
+                    $locations = Locations::query()->where('id_fs', $locations['id'])->update([
+                        'id_fs' => $locations['id'],
+                        'name' => $locations['attributes']['name']
+                    ]);
+                }
+
+            }
+
+            
+        }
+
+        $locationsOnDB =  Locations::query()->where('id_fs', '<>', 3226)->orderBy('name', 'asc')->get()->pluck('id_fs')->toArray();
+
+        foreach($locationsOnDB as $onDB){
+            if(!in_array($onDB, $locationsOnFS)){
+                print('Si existe');
+            }
         }
     }
 }
